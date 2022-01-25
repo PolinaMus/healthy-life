1cdffbc28580e4c024582c757012bdeeb2042396
Polina <oligarh.sun@mail.ru>
2022-01-25 23:29:18 +0300

    ci: github actions added

---
+++ src/test/java/com/company/ItemCRUDTest.java
@@ -0,0 +1,169 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.nio.file.Path;
+
+@Testcontainers
+@SpringBootTest
+@DirtiesContext
+@AutoConfigureMockMvc
+class ItemCRUDTest {
+    @Container
+    static DockerComposeContainer<?> compose = new DockerComposeContainer<>(
+            Path.of("docker-compose.yml").toFile()
+    );
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldPerformCRUD() throws Exception {
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/items/getAll")
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.content().json(
+                                // language=JSON
+                                """
+                                        {
+                                        "items": [
+                                            {
+                                              "id": 1,
+                                              "name": "vitamin d",
+                                              "price": 500,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            },
+                                            {
+                                              "id": 2,
+                                              "name": "vitamin c",
+                                              "price": 650,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            },
+                                            {
+                                              "id": 3,
+                                              "name": "vitamin a",
+                                              "price": 900,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            },
+                                            {
+                                              "id": 4,
+                                              "name": "vitamin e",
+                                              "price": 1100,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            },
+                                            {
+                                              "id": 5,
+                                              "name": "b complex",
+                                              "price": 1300,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            },
+                                            {
+                                              "id": 6,
+                                              "name": "magnesium",
+                                              "price": 1200,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            },
+                                            {
+                                              "id": 7,
+                                              "name": "calcium",
+                                              "price": 750,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            },
+                                            {
+                                              "id": 8,
+                                              "name": "collagen",
+                                              "price": 1500,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            },
+                                            {
+                                              "id": 9,
+                                              "name": "omega 3",
+                                              "price": 1900,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            },
+                                            {
+                                              "id": 10,
+                                              "name": "probiotics",
+                                              "price": 1500,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            },
+                                            {
+                                              "id": 11,
+                                              "name": "multivitamin",
+                                              "price": 2000,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            },
+                                            {
+                                              "id": 12,
+                                              "name": "enzymes",
+                                              "price": 1700,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            },
+                                            {
+                                              "id": 13,
+                                              "name": "protein",
+                                              "price": 1350,
+                                              "qty": 500,
+                                              "image": "noimage.png"
+                                            }
+                                          ]
+                                          }
+                                        """
+                        )
+                );
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/items/getById")
+                                .param("id", String.valueOf(2))
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.content().json(
+                                // language=JSON
+                                """
+                                        {
+                                          "item": {
+                                            "id": 2,
+                                            "name": "vitamin c",
+                                            "price": 650,
+                                            "qty": 500,
+                                            "image": "noimage.png"
+                                          }
+                                        }
+                                        """
+                        )
+                );
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/items/getById")
+                                .param("id", String.valueOf(999))
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isNotFound()
+                );
+    }
+}
