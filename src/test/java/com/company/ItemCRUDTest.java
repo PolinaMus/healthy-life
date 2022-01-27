14e024fc3a884e2493386fad36500666776cf394
Polina <oligarh.sun@mail.ru>
2022-01-27 22:37:02 +0300

    added fixes

--- src/test/java/com/company/ItemCRUDTest.java
+++ src/test/java/com/company/ItemCRUDTest.java
@@ -39,100 +39,100 @@ class ItemCRUDTest {
                                 // language=JSON
                                 """
                                         {
-                                        "items": [
-                                            {
-                                              "id": 1,
-                                              "name": "vitamin d",
-                                              "price": 500,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            },
-                                            {
-                                              "id": 2,
-                                              "name": "vitamin c",
-                                              "price": 650,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            },
-                                            {
-                                              "id": 3,
-                                              "name": "vitamin a",
-                                              "price": 900,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            },
-                                            {
-                                              "id": 4,
-                                              "name": "vitamin e",
-                                              "price": 1100,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            },
-                                            {
-                                              "id": 5,
-                                              "name": "b complex",
-                                              "price": 1300,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            },
-                                            {
-                                              "id": 6,
-                                              "name": "magnesium",
-                                              "price": 1200,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            },
-                                            {
-                                              "id": 7,
-                                              "name": "calcium",
-                                              "price": 750,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            },
-                                            {
-                                              "id": 8,
-                                              "name": "collagen",
-                                              "price": 1500,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            },
-                                            {
-                                              "id": 9,
-                                              "name": "omega 3",
-                                              "price": 1900,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            },
-                                            {
-                                              "id": 10,
-                                              "name": "probiotics",
-                                              "price": 1500,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            },
-                                            {
-                                              "id": 11,
-                                              "name": "multivitamin",
-                                              "price": 2000,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            },
-                                            {
-                                              "id": 12,
-                                              "name": "enzymes",
-                                              "price": 1700,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            },
-                                            {
-                                              "id": 13,
-                                              "name": "protein",
-                                              "price": 1350,
-                                              "qty": 500,
-                                              "image": "noimage.png"
-                                            }
-                                          ]
-                                          }
+                                             "items": [
+                                               {
+                                                 "id": 1,
+                                                 "name": "vitamin d",
+                                                 "price": 500,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               },
+                                               {
+                                                 "id": 2,
+                                                 "name": "vitamin c",
+                                                 "price": 650,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               },
+                                               {
+                                                 "id": 3,
+                                                 "name": "vitamin a",
+                                                 "price": 900,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               },
+                                               {
+                                                 "id": 4,
+                                                 "name": "vitamin e",
+                                                 "price": 1100,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               },
+                                               {
+                                                 "id": 5,
+                                                 "name": "b complex",
+                                                 "price": 1300,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               },
+                                               {
+                                                 "id": 6,
+                                                 "name": "magnesium",
+                                                 "price": 1200,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               },
+                                               {
+                                                 "id": 7,
+                                                 "name": "calcium",
+                                                 "price": 750,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               },
+                                               {
+                                                 "id": 8,
+                                                 "name": "collagen",
+                                                 "price": 1500,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               },
+                                               {
+                                                 "id": 9,
+                                                 "name": "omega 3",
+                                                 "price": 1900,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               },
+                                               {
+                                                 "id": 10,
+                                                 "name": "probiotics",
+                                                 "price": 1500,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               },
+                                               {
+                                                 "id": 11,
+                                                 "name": "multivitamin",
+                                                 "price": 2000,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               },
+                                               {
+                                                 "id": 12,
+                                                 "name": "enzymes",
+                                                 "price": 1700,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               },
+                                               {
+                                                 "id": 13,
+                                                 "name": "protein",
+                                                 "price": 1350,
+                                                 "qty": 500,
+                                                 "image": "noimage.png"
+                                               }
+                                             ]
+                                           }
                                         """
                         )
                 );
@@ -216,23 +216,23 @@ class ItemCRUDTest {
                                             },
                                             {
                                               "id": 5,
-                                              "count": 2,
+                                              "count": 3,
                                               "name": "b complex"
                                             },
                                             {
                                               "id": 2,
-                                              "count": 2,
+                                              "count": 3,
                                               "name": "vitamin c"
+                                            },
+                                            {
+                                              "id": 1,
+                                              "count": 2,
+                                              "name": "vitamin d"
                                             },
                                             {
                                               "id": 6,
                                               "count": 1,
                                               "name": "magnesium"
-                                            },
-                                            {
-                                              "id": 1,
-                                              "count": 1,
-                                              "name": "vitamin d"
                                             }
                                           ]
                                         }
