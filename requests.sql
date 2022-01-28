c84c61ffd08213f85c5120ca5acfbdd47b3054f6
Polina <oligarh.sun@mail.ru>
2022-01-28 22:02:10 +0300

    fix: added fixes

--- requests.sql
+++ requests.sql
@@ -1,49 +1,51 @@
-SELECT id, name, price AS total
+-- Select 5 Vitamins where price ranging from 500 to 1500
+SELECT id, name vitamin, price AS total
 FROM items
-WHERE price BETWEEN 500 AND 1100
-ORDER BY price ASC;
+WHERE price BETWEEN 500 AND 1500
+ORDER BY price ASC
+LIMIT 5;
 
-
+-- The amount of Vitamins from the first position of the selection
 WITH item_freq AS (
     SELECT first_item_id item_id, count(*) count
     FROM articles
     GROUP BY first_item_id
 )
-SELECT if.count, if.item_id first_item_id, i.name
+SELECT if.count, if.item_id first_item_id, i.name vitamin
 FROM item_freq if
          LEFT JOIN items i ON if.item_id = i.id
 ORDER BY if.count DESC;
 
-
+-- The amount of Vitamins from the second position of the selection (5 vitamins)
 WITH item_freq AS (
     SELECT second_item_id item_id, count(*) count
     FROM articles
     GROUP BY second_item_id
 )
-SELECT if.count, if.item_id second_item_id, i.name
+SELECT if.count, if.item_id second_item_id, i.name vitamin
 FROM item_freq if
          LEFT JOIN items i ON if.item_id = i.id
 ORDER BY if.count DESC
 LIMIT 5;
 
-
+-- The amount of Vitamins from the third position of the selection
 WITH item_freq AS (
     SELECT third_item_id item_id, count(*) count
     FROM articles
     GROUP BY third_item_id
 )
-SELECT if.count, if.item_id third_item_id, i.name
+SELECT if.count, if.item_id third_item_id, i.name vitamin
 FROM item_freq if
          LEFT JOIN items i ON if.item_id = i.id
 ORDER BY if.count DESC;
 
-
-SELECT a.name article_name, a.first_item_id, i.name vitamin_name
+-- Articles and selection of Vitamins from the first position
+SELECT a.name article, a.first_item_id, i.name vitamin
 FROM items i
          INNER JOIN articles a ON a.first_item_id = i.id;
 
-
-SELECT a.name,
+-- Selection of articles, article categories and vitamins
+SELECT a.name article,
        a.category,
        i1.name first_vitamin_name,
        i2.name second_vitamin_name,
@@ -53,7 +55,7 @@ FROM articles a
          LEFT JOIN items i2 on i2.id = a.second_item_id
          LEFT JOIN items i3 on i3.id = a.third_item_id;
 
-
+-- Top 5 vitamins
 WITH item_freq AS (
     SELECT first_item_id item_id, count(*) count
     FROM articles
@@ -67,7 +69,7 @@ WITH item_freq AS (
     FROM articles
     GROUP BY third_item_id
 )
-SELECT if.item_id id, i.name, if.count
+SELECT if.item_id id, i.name vitamin, if.count
 FROM item_freq if
          LEFT JOIN items i ON if.item_id = i.id
 ORDER BY if.count DESC
--- src/test/java/com/company/SaleTest.java
+++ src/test/java/com/company/SaleTest.java
@@ -1,144 +1,144 @@
-//package com.company;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.annotation.DirtiesContext;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-//import org.testcontainers.containers.DockerComposeContainer;
-//import org.testcontainers.junit.jupiter.Container;
-//import org.testcontainers.junit.jupiter.Testcontainers;
-//
-//import java.nio.file.Path;
-//
-//@Testcontainers
-//@SpringBootTest
-//@DirtiesContext
-//@AutoConfigureMockMvc
-//class SaleTest {
-//    @Container
-//    static DockerComposeContainer<?> compose = new DockerComposeContainer<>(
-//            Path.of("docker-compose.yml").toFile()
-//    );
-//
-//    @Autowired
-//    MockMvc mockMvc;
-//
-//    @Test
-//    void shouldPerformSale() throws Exception {
-//        mockMvc.perform(
-//                        MockMvcRequestBuilders.post("/sales/register")
-//                                .contentType(MediaType.APPLICATION_JSON)
-//                                .content(
-//                                        // language=JSON
-//                                        """
-//                                                {
-//                                                "itemId": 9,
-//                                                "price": 1900,
-//                                                "qty": 10
-//                                                }
-//                                                """
-//                                )
-//                )
-//                .andExpectAll(
-//                        MockMvcResultMatchers.status().isOk(),
-//                        MockMvcResultMatchers.content().json(
-//                                // language=JSON
-//                                """
-//                                        {
-//                                        "sale": {
-//                                        "id": 1,
-//                                        "itemId": 9,
-//                                        "name": "omega 3",
-//                                        "price": 1900,
-//                                        "qty": 10
-//                                        }
-//                                        }
-//                                        """
-//                        )
-//                );
-//
-//        mockMvc.perform(
-//                        MockMvcRequestBuilders.get("/items/getById")
-//                                .param("id", String.valueOf(9))
-//                )
-//                .andExpectAll(
-//                        MockMvcResultMatchers.status().isOk(),
-//                        MockMvcResultMatchers.content().json(
-//                                // language=JSON
-//                                """
-//                                        {
-//                                          "item": {
-//                                            "id": 9,
-//                                            "name": "omega 3",
-//                                            "price": 1900,
-//                                            "qty": 490,
-//                                            "image": "noimage.png"
-//                                          }
-//                                        }
-//                                        """
-//                        )
-//                );
-//
-//        mockMvc.perform(
-//                        MockMvcRequestBuilders.get("/sales/getAll")
-//                )
-//                .andExpectAll(
-//                        MockMvcResultMatchers.status().isOk(),
-//                        MockMvcResultMatchers.content().json(
-//                                // language=JSON
-//                                """
-//                                        {
-//                                          "sales": [
-//                                            {
-//                                              "id": 1,
-//                                              "name": "omega 3",
-//                                              "price": 1900,
-//                                              "qty": 10
-//                                            },
-//                                            {
-//                                              "id": 2,
-//                                              "name": "vitamin a",
-//                                              "price": 900,
-//                                              "qty": 2
-//                                            }
-//                                          ]
-//                                        }
-//                                        """
-//                        )
-//                );
-//
-//        mockMvc.perform(
-//                        MockMvcRequestBuilders.get("/sales/getById")
-//                                .param("id", String.valueOf(2))
-//                )
-//                .andExpectAll(
-//                        MockMvcResultMatchers.status().isOk(),
-//                        MockMvcResultMatchers.content().json(
-//                                // language=JSON
-//                                """
-//                                        {
-//                                          "sale": {
-//                                            "id": 2,
-//                                            "name": "vitamin a",
-//                                            "price": 900,
-//                                            "qty": 2
-//                                          }
-//                                        }
-//                                        """
-//                        )
-//                );
-//
-//        mockMvc.perform(
-//                        MockMvcRequestBuilders.get("/sales/getById")
-//                                .param("id", String.valueOf(999))
-//                )
-//                .andExpectAll(
-//                        MockMvcResultMatchers.status().isNotFound()
-//                );
-//    }
-//}
+package com.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.nio.file.Path;
+
+@Testcontainers
+@SpringBootTest
+@DirtiesContext
+@AutoConfigureMockMvc
+class SaleTest {
+    @Container
+    static DockerComposeContainer<?> compose = new DockerComposeContainer<>(
+            Path.of("docker-compose.yml").toFile()
+    );
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldPerformSale() throws Exception {
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/sales/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(
+                                        // language=JSON
+                                        """
+                                                {
+                                                "itemId": 9,
+                                                "price": 1900,
+                                                "qty": 10
+                                                }
+                                                """
+                                )
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.content().json(
+                                // language=JSON
+                                """
+                                        {
+                                        "sale": {
+                                        "id": 1,
+                                        "itemId": 9,
+                                        "name": "omega 3",
+                                        "price": 1900,
+                                        "qty": 10
+                                        }
+                                        }
+                                        """
+                        )
+                );
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/items/getById")
+                                .param("id", String.valueOf(9))
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.content().json(
+                                // language=JSON
+                                """
+                                        {
+                                          "item": {
+                                            "id": 9,
+                                            "name": "omega 3",
+                                            "price": 1900,
+                                            "qty": 490,
+                                            "image": "noimage.png"
+                                          }
+                                        }
+                                        """
+                        )
+                );
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/sales/getAll")
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.content().json(
+                                // language=JSON
+                                """
+                                        {
+                                          "sales": [
+                                            {
+                                              "id": 1,
+                                              "name": "omega 3",
+                                              "price": 1900,
+                                              "qty": 10
+                                            },
+                                            {
+                                              "id": 2,
+                                              "name": "vitamin a",
+                                              "price": 900,
+                                              "qty": 2
+                                            }
+                                          ]
+                                        }
+                                        """
+                        )
+                );
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/sales/getById")
+                                .param("id", String.valueOf(2))
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.content().json(
+                                // language=JSON
+                                """
+                                        {
+                                          "sale": {
+                                            "id": 2,
+                                            "name": "vitamin a",
+                                            "price": 900,
+                                            "qty": 2
+                                          }
+                                        }
+                                        """
+                        )
+                );
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/sales/getById")
+                                .param("id", String.valueOf(999))
+                )
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isNotFound()
+                );
+    }
+}
